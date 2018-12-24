a1e2b08986a0cf39fff0b183161ebfd4b50aa289
lianchao <18900945189@163.com>
2018-12-24 21:44:32 +0800

    数据库的链接配置

---
+++ config/mast/DataSourceConfig.java
@@ -0,0 +1,38 @@
+package com.pp.learn.demo.config.mast;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+
+    //对接前置库
+    @Bean
+    @Primary
+    public DataSource sqliteDataSource() throws ClassNotFoundException {
+        Class.forName(driverClassName);
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(datasourceUrl);
+        config.setUsername("root");
+        config.setPassword("root");
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JdbcTemplate sqliteJdbcTemplate() throws ClassNotFoundException {
+        return new JdbcTemplate(sqliteDataSource());
+    }
+}
